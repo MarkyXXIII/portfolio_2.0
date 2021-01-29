1d4e1c1f944bd94e0961f0a99b1e6f84da9a700d
MarkyXXIII <74665566+MarkyXXIII@users.noreply.github.com>
2021-01-29 15:46:54 +0000

    Edits. Lots.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # portfolio_2.0
-Second attempt at a portfolio website
+Second attempt at a portfolio website. I have taken a template that includes two technologies that are new to me - Bootstrap and jQuery - and practiced reading/editing code.
--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -1,5 +1,5 @@
 const initBg = (autoplay = true) => {
-    const bgImgsNames = ['diagoona-bg-1.jpg', 'diagoona-bg-2.jpg', 'diagoona-bg-3.jpg'];
+    const bgImgsNames = ['wp4.png', 'wp7.png', 'wp10.png'];
     const bgImgs = bgImgsNames.map(img => "img/" + img);
 
     $.backstretch(bgImgs, {duration: 4000, fade: 500});
